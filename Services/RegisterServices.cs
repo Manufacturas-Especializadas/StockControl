1db4c9afd1feb8e2bad42fb0e6a75e37e236eff6
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2024-04-02 14:21:44 -0600

    Tabla usuarios

--- Services/RegisterServices.cs
+++ Services/RegisterServices.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StockControl.Models;
+using System.Diagnostics;
 
 namespace StockControl.Services
 {
@@ -15,6 +16,32 @@ namespace StockControl.Services
         public async Task<List<Rol>>GetRol()
         {
             return await _context.Rols.ToListAsync();
+        }
+
+        public async Task<int> GetTotalPagesAsync(int PageSize)
+        {
+            var totalItems = await _context.Registers.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalItems / (double)PageSize);
+            return totalPages;
+        }
+
+        public async Task<List<Register>> GetPagedUsers(int PageNumber, int PageSize)
+        {
+            try
+            {
+                var skip = PageNumber == 1 ? 0 : (PageNumber - 1) * PageSize;
+                return await _context.Registers
+                    .Include(r => r.FkRolNavigation)
+                    .OrderBy(s => s.Id)
+                    .Skip(skip)
+                    .Take(PageSize)
+                    .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            return new List<Register>();
         }
 
         public async Task<Register> CREATE(Register register)
